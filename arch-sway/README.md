bfcfd884b319d5b2e6bd74cfe8d59ffa168b9168
Luthfi Tri Atmaja <luthfi.personal@gmail.com>
2024-11-11 03:23:14 +0700

    feat(arch-sway): rework installation script

--- arch-sway/README.md
+++ arch-sway/README.md
@@ -2,45 +2,11 @@
 
 A collection of configuration files for Arch Linux (minimal install) and Sway as the window manager.
 
-## Installation
+The installation script is intended for a fresh Arch Linux installation only. It will install and configure a range of software and settings. Once the script finishes, reboot the system.
 
-The installation script is intended for a fresh Arch Linux installation only. It will install and configure a range of software and settings.
-
-Note that the script installs `Oh My Zsh`, which may change the default shell to `zsh`. After installation, type `exit` to return to `bash` and continue with the rest of the process.
-
-1. Clone the repository and navigate to the installation directory:
-
-    ```shell
-    git clone https://github.com/luthfiampas/dotconfig && \
-    cd dotconfig/arch-sway
-    ```
-
-2. Run the installation script:
-
-    ```shell
-    ./_install.sh
-    ```
-
-3. Once the script finishes, reboot the system.
-
-## Additional Steps
-
-### Install `Firefox-UI-Fix`
-
-Apply the `photon` UI style to Firefox.
 
 ```shell
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/black7375/Firefox-UI-Fix/master/install.sh)"
-```
-
-### Install Python and Node.js
-
-Install Python and Node.js using `pyenv` and `nvm`.
-
-```shell
-pyenv install <version>
-pyenv global <version>
-
-nvm install <version>
-nvm use <version>
+git clone https://github.com/luthfiampas/dotconfig && \
+cd dotconfig/arch-sway && \
+./_install.sh
 ```
--- arch-sway/_install.sh
+++ arch-sway/_install.sh
@@ -1,271 +1,386 @@
 #!/bin/bash
 
-# Exit the script on error (for the critical parts)
 set -e
 
-# Perform system update
+install_zsh() {
+    echo "Installing Zsh and dependencies..."
+    sudo pacman -S --needed --noconfirm zsh git curl
+
+    echo "Installing Oh My Zsh..."
+    RUNZSH=no sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+    echo "Zsh installed successfully!"
+}
+
+install_yay() {
+    echo "Installing yay..."
+    sudo pacman -S --needed --noconfirm base-devel git
+    git clone https://aur.archlinux.org/yay.git
+    pushd yay
+    makepkg -si --noconfirm
+    popd
+    rm -rf yay
+    echo "yay has been installed successfully!"
+}
+
+install_pyenv() {
+    echo "Installing pyenv..."
+    sudo pacman -S --needed --noconfirm gcc make libssl zlib git
+    curl https://pyenv.run | bash
+    echo "pyenv installed successfully!"
+}
+
+install_nvm() {
+    echo "Installing nvm..."
+    sudo pacman -S --needed --noconfirm curl
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+    echo "nvm installed successfully!"
+}
+
+install_lutlight() {
+    echo "Building lutlight..."
+    mkdir -p ~/.bin
+    gcc -O2 -o ~/.bin/lutlight _src/lutlight/lutlight.c -lm
+    echo "lutlight built successfully and saved to ~/.bin/lutlight."
+}
+
+install_essential_packages() {
+    echo "Installing essential packages..."
+
+    yay -S --noconfirm --needed \
+        alacritty \
+        aspnet-runtime \
+        bat \
+        blender \
+        btop \
+        calc \
+        ddcutil \
+        dbeaver \
+        dnscrypt-proxy \
+        dotnet-sdk \
+        easyeffects \
+        ffmpeg \
+        fzf \
+        glow \
+        grim \
+        imv \
+        iwd \
+        light \
+        mpv \
+        ncdu \
+        noto-fonts-cjk \
+        noto-fonts-emoji \
+        ntfs-3g \
+        openssh \
+        polkit-gnome \
+        pipewire \
+        pipewire-alsa \
+        pipewire-audio \
+        pipewire-jack \
+        pipewire-pulse \
+        pavucontrol \
+        powerlevel10k-git \
+        qt6-wayland \
+        grim \
+        sqlitestudio-bin \
+        samba \
+        sway \
+        swaybg \
+        swappy \
+        ttf-font-awesome \
+        ttf-jetbrains-mono \
+        ttf-jetbrains-mono-nerd \
+        ttf-roboto \
+        ttf-roboto-serif \
+        vim \
+        visual-studio-code-bin \
+        waybar \
+        wofi \
+        xdg-desktop-portal \
+        xdg-desktop-portal-wlr \
+        xorg-xwayland
+
+    echo "Essential packages installed successfully!"
+}
+
+make_essential_directories() {
+    echo "Creating essential directories..."
+
+    sudo mkdir -p /data/pictures/wallpapers
+    sudo mkdir -p /data/repos/personal
+    sudo mkdir -p /data/repos/client
+    sudo mkdir -p /mnt/1
+    sudo mkdir -p /mnt/2
+    sudo mkdir -p /mnt/3
+
+    sudo chmod 777 -R /data
+    sudo chmod 777 -R /mnt/1
+    sudo chmod 777 -R /mnt/2
+    sudo chmod 777 -R /mnt/3
+
+    echo "Essential directories created successfully!"
+}
+
+copy_root() {
+    echo "Copying root folder contents to system root..."
+    sudo cp -r root/. /
+    echo "Root contents copied successfully!"
+}
+
+copy_dotconfig() {
+    echo "Copying .config directory..."
+    mkdir -p ~/.config
+    cp -r .config/. ~/.config/
+    echo ".config directory copied successfully!"
+}
+
+copy_dotfiles() {
+    echo "Copying dot files..."
+    cp .bash_profile ~/.
+    cp .bashrc ~/.
+    cp .dircolors ~/.
+    cp .gtkrc-2.0 ~/.
+    cp .p10k.zsh ~/.
+    cp .vimrc ~/.
+    cp .zshrc ~/.
+    cp .zprofile ~/.
+    echo "Dot files copied successfully!"
+}
+
+copy_bin_files() {
+    echo "Copying bin files..."
+    mkdir -p ~/.bin
+    cp -r .bin/. ~/.bin/
+    chmod +x ~/.bin/clean-desktop
+    echo "Bin files copied!"
+}
+
+copy_ssh_config() {
+    echo "Copying SSH config file..."
+    mkdir -p ~/.ssh
+
+    cp .ssh/config ~/.ssh/
+    chmod 600 ~/.ssh/config
+
+    echo "SSH config file copied successfully!"
+}
+
+setup_services() {
+    echo "Setting up dnscrypt-proxy service..."
+    if sudo systemctl enable --now dnscrypt-proxy; then
+        echo "dnscrypt-proxy service started successfully."
+    else
+        echo "Failed to start dnscrypt-proxy service, but continuing..."
+    fi
+
+    echo "Setting up ssh-agent service..."
+    if systemctl --user enable --now ssh-agent; then
+        echo "ssh-agent service started successfully."
+    else
+        echo "Failed to start ssh-agent service."
+    fi
+
+    echo "Service setup complete. If any issues occurred, please review the error messages above."
+}
+
+configure_zsh() {
+    echo "Defining plugin and theme path..."
+    ZSH_CUSTOM="${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"
+
+    echo "Installing Zsh plugins and themes..."
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "$ZSH_CUSTOM/themes/powerlevel10k"
+    git clone https://github.com/zsh-users/zsh-autosuggestions "$ZSH_CUSTOM/plugins/zsh-autosuggestions"
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git "$ZSH_CUSTOM/plugins/zsh-syntax-highlighting"
+    git clone https://github.com/fdellwing/zsh-bat.git "$ZSH_CUSTOM/plugins/zsh-bat"
+
+    echo "Zsh plugins and themes installed successfully!"
+}
+
+configure_vim() {
+    echo "Installing vim-plug..."
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+    echo "Copying .vimrc..."
+    cp .vimrc ~/.
+
+    echo "Installing vim plugins..."
+    vim -N -es -c 'PlugInstall!' -c 'qa'
+    echo "Vim plugins installed successfully!"
+}
+
+configure_samba() {
+    echo "Configuring Samba..."
+
+    if ! getent group smbusers >/dev/null 2>&1; then
+        echo "Creating smbusers group..."
+        sudo groupadd smbusers
+    else
+        echo "smbusers group already exists."
+    fi
+
+    echo "Adding user to smbusers group..."
+    sudo usermod -aG smbusers "$USER"
+
+    echo "Setting Samba password for $USER..."
+    if echo -e "$samba_password\n$samba_password" | sudo smbpasswd -a "$USER"; then
+        echo "Samba password set successfully!"
+    else
+        echo "Failed to set Samba password!" >&2
+        exit 1
+    fi
+
+    echo "Samba configured successfully!"
+}
+
+configure_docker() {
+    echo "Configuring Docker..."
+
+    if id -nG "$USER" | grep -qw "docker"; then
+        echo "$USER is already a member of the docker group."
+    else
+        sudo usermod -aG docker "$USER"
+        echo "$USER added to docker group."
+    fi
+
+    echo "Docker configured successfully!"
+}
+
+configure_light() {
+    echo "Configuring light..."
+
+    if id -nG "$USER" | grep -qw "video"; then
+        echo "$USER is already a member of the video group."
+    else
+        sudo usermod -aG video "$USER"
+        echo "$USER added to video group."
+    fi
+
+    echo "Light configured successfully!"
+}
+
+configure_git() {
+    echo "Configuring Git..."
+
+    git config --global user.name "$git_username"
+    git config --global user.email "$git_email"
+    git config --global core.editor "$git_editor"
+
+    echo "Git configuration updated:"
+    echo "user.name: $(git config --global user.name)"
+    echo "user.email: $(git config --global user.email)"
+    echo "core.editor: $(git config --global core.editor)"
+
+    echo "Git configured successfully!"
+}
+
+configure_dotnet() {
+    echo "Installing global dotnet tools..."
+    dotnet tool install --global dotnet-ef
+    dotnet tool install --global csharprepl
+
+    echo "Installing dotnet templates..."
+    dotnet new install Avalonia.Templates
+
+    echo "Global dotnet tools and templates installed successfully!"
+}
+
+configure_vs_code() {
+    echo "Installing Visual Studio Code extensions..."
+
+    code --install-extension codeium.codeium
+    code --install-extension devsense.phptools-vscode
+    code --install-extension eamodio.gitlens
+    code --install-extension editorconfig.editorconfig
+    code --install-extension esbenp.prettier-vscode
+    code --install-extension fcrespo82.markdown-table-formatter
+    code --install-extension foxundermoon.shell-format
+    code --install-extension gruntfuggly.todo-tree
+    code --install-extension hediet.vscode-drawio
+    code --install-extension miguelsolorio.symbols
+    code --install-extension ms-dotnettools.csharp@2.45.25
+    code --install-extension redhat.vscode-yaml
+    code --install-extension rogalmic.vscode-xml-complete
+    code --install-extension shd101wyy.markdown-preview-enhanced
+    code --install-extension sissel.shopify-liquid
+    code --install-extension sleistner.vscode-fileutils
+    code --install-extension tamasfe.even-better-toml
+    code --install-extension yinz.safira
+
+    echo "Visual Studio Code configured successfully!"
+}
+
+configure_python() {
+    echo "Installing Python 3.12.7 from pyenv..."
+    zsh -c "source ~/.zshrc && pyenv install 3.12.7 && pyenv global 3.12.7" || echo "Python installation failed"
+    echo "Python 3.12.7 installed successfully!"
+}
+
+configure_node_js() {
+    echo "Installing Node.js LTS from nvm..."
+    zsh -c "source ~/.zshrc && nvm install --lts && nvm use --lts" || echo "Node.js LTS installation failed"
+    echo "Node.js LTS installed successfully!"
+}
+
+clean_up() {
+    echo "Cleaning cache..."
+    sudo rm -rf ~/.cache/* || true
+
+    echo "Cleaning go config..."
+    rm -rf ~/.config/go || true
+
+    echo "Cleaning desktop entries..."
+    if ! .bin/clean-desktop; then
+        echo "Failed to clean desktop entries"
+    fi
+
+    echo "Cleaning up AUR packages and cache..."
+    yay -Yc --noconfirm || true
+    yay -Sc --noconfirm || true
+
+    echo "Cleaning up temporary files..."
+    sudo rm -rf /tmp/* || true
+}
+
+echo "Enter Git user name:"
+read git_username
+
+echo "Enter Git email address:"
+read git_email
+
+echo "Enter Git preferred text editor (e.g., vim, nano, code):"
+read git_editor
+
+echo "Enter a Samba password for $USER:"
+read -s samba_password
+
 echo "Updating system..."
 sudo pacman -Syu --noconfirm
 
-# Install yay (AUR helper)
-echo "Installing yay (AUR helper)..."
-sudo pacman -S --needed git base-devel && \
-git clone https://aur.archlinux.org/yay.git && \
-cd yay && \
-makepkg -si --noconfirm && \
-cd .. && \
-rm -rf yay
+install_zsh
+install_yay
+install_pyenv
+install_nvm
+install_lutlight
+install_essential_packages
 
-# Clean-up unnecessary files (recheck before deletion)
-echo "Cleaning up unnecessary files..."
-rm -rf ~/.config/go
+make_essential_directories
+copy_root
+copy_dotconfig
+copy_dotfiles
+copy_bin_files
+copy_ssh_config
 
-# Install pyenv
-echo "Installing pyenv..."
-curl https://pyenv.run | bash
+setup_services
+configure_zsh
+configure_vim
+configure_samba
+configure_docker
+configure_light
+configure_git
+configure_dotnet
+configure_vs_code
+configure_python
+configure_node_js
 
-# Install nvm
-echo "Installing nvm..."
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
-
-# Install initial packages from AUR and official repositories
-echo "Installing initial packages..."
-yay -S --noconfirm --needed \
-ttf-roboto \
-ttf-roboto-serif \
-ttf-jetbrains-mono \
-ttf-jetbrains-mono-nerd \
-ttf-font-awesome \
-noto-fonts-emoji \
-noto-fonts-cjk \
-iwd \
-zip \
-unzip \
-vim \
-ncdu \
-zsh \
-fzf \
-bat \
-btop \
-ntfs-3g \
-glow \
-openssh \
-pipewire \
-pipewire-audio \
-pipewire-alsa \
-pipewire-jack \
-pipewire-pulse \
-alacritty \
-sway \
-swaybg \
-wofi \
-waybar \
-xorg-xwayland \
-polkit-gnome \
-xdg-desktop-portal \
-xdg-desktop-portal-wlr \
-fastfetch \
-calc \
-light \
-ddcutil \
-grim \
-slurp \
-swappy \
-imv \
-mpv \
-ffmpeg \
-dnscrypt-proxy \
-pavucontrol \
-samba \
-visual-studio-code-bin \
-firefox \
-dbeaver \
-sqlitestudio-bin \
-dotnet-sdk \
-aspnet-runtime \
-docker \
-obs-studio \
-qt6-wayland \
-blender \
-easyeffects \
-qemu-full \
-virt-manager
-
-# Install Oh My Zsh
-echo "Installing Oh My Zsh..."
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install Oh My Zsh plugins and themes
-echo "Installing Zsh plugins and themes..."
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/fdellwing/zsh-bat.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-bat
-
-# Try to start dnscrypt-proxy and continue even if an error occurs
-echo "Setting up dnscrypt-proxy service..."
-sudo systemctl enable --now dnscrypt-proxy || true
-
-# Build lutlight
-echo "Building lutlight..."
-mkdir -p ~/.bin
-gcc -O2 -o ~/.bin/lutlight _src/lutlight/lutlight.c -lm
-
-# Ensure ~/.bin exists before copying
-echo "Copying bin files..."
-mkdir -p ~/.bin
-cp .bin/clean-desktop ~/.bin/. && chmod +x ~/.bin/clean-desktop
-
-# Create commonly used directories
-echo "Creating directories..."
-sudo mkdir -p /data/pictures/wallpapers
-sudo mkdir -p /data/repos/personal
-sudo mkdir -p /data/repos/client
-sudo mkdir -p /mnt/1
-sudo mkdir -p /mnt/2
-sudo mkdir -p /mnt/3
-sudo chmod 777 -R /data
-sudo chmod 777 -R /mnt/1
-sudo chmod 777 -R /mnt/2
-sudo chmod 777 -R /mnt/3
-
-# Copy wallpapers
-echo "Copying wallpapers..."
-cp root/data/pictures/wallpapers/* /data/pictures/wallpapers/.
-
-# Copy necessary files to root
-echo "Copying necessary files to root..."
-sudo cp root/etc/resolv.conf /etc/resolv.conf
-sudo mkdir -p /etc/systemd
-sudo cp root/etc/systemd/resolved.conf /etc/systemd/resolved.conf
-sudo mkdir -p /etc/dnscrypt-proxy
-sudo cp root/etc/dnscrypt-proxy/dnscrypt-proxy.toml /etc/dnscrypt-proxy/dnscrypt-proxy.toml
-sudo mkdir -p /etc/samba
-sudo cp root/etc/samba/smb.conf /etc/samba/smb.conf
-sudo mkdir -p /boot/loader
-sudo cp root/boot/loader/loader.conf /boot/loader/loader.conf
-sudo cp root/usr/share/applications/firefox-private.desktop /usr/share/applications/firefox-private.desktop
-
-# Copy dot files to home directory
-echo "Copying dot files..."
-cp .bash_profile ~/.
-cp .bashrc ~/.
-cp .dircolors ~/.
-cp .gtkrc-2.0 ~/.
-cp .p10k.zsh ~/.
-cp .vimrc ~/.
-cp .zprofile ~/.
-cp .zshrc ~/.
-
-# Copy SSH config file
-echo "Copying SSH config file..."
-mkdir -p ~/.ssh && cp .ssh/config ~/.ssh/.
-
-# Copy .config files and directories
-echo "Copying .config files..."
-cp -r .config/alacritty ~/.config/.
-cp -r .config/Code ~/.config/.
-cp -r .config/fontconfig ~/.config/.
-cp -r .config/gtk-2.0 ~/.config/.
-cp -r .config/gtk-3.0 ~/.config/.
-cp -r .config/gtk-4.0 ~/.config/.
-cp -r .config/light ~/.config/.
-cp -r .config/sway ~/.config/.
-cp -r .config/systemd ~/.config/.
-cp -r .config/waybar ~/.config/.
-cp -r .config/wofi ~/.config/.
-cp -r .config/xdg-desktop-portal ~/.config/.
-cp -r .config/btop ~/.config/.
-cp -r .config/glow ~/.config/.
-cp .config/user-dirs.dirs ~/.config/.
-
-# Create and start ssh-agent service
-echo "Setting up ssh-agent service..."
-systemctl --user enable --now ssh-agent
-
-# Install vim-plug
-echo "Installing vim-plug..."
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-# Install vim plugins
-echo "Installing vim plugins..."
-vim -N -es -c 'PlugInstall!' -c 'qa'
-
-# Install commonly used VSCode extensions
-echo "Installing VSCode extensions..."
-code --install-extension codeium.codeium && \
-code --install-extension devsense.phptools-vscode && \
-code --install-extension eamodio.gitlens && \
-code --install-extension editorconfig.editorconfig && \
-code --install-extension esbenp.prettier-vscode && \
-code --install-extension fcrespo82.markdown-table-formatter && \
-code --install-extension gruntfuggly.todo-tree && \
-code --install-extension hediet.vscode-drawio && \
-code --install-extension miguelsolorio.symbols && \
-code --install-extension ms-dotnettools.csharp@2.45.25 && \
-code --install-extension redhat.vscode-yaml && \
-code --install-extension rogalmic.vscode-xml-complete && \
-code --install-extension shd101wyy.markdown-preview-enhanced && \
-code --install-extension sissel.shopify-liquid && \
-code --install-extension sleistner.vscode-fileutils && \
-code --install-extension tamasfe.even-better-toml && \
-code --install-extension yinz.safira
-
-# Configure samba
-echo "Configuring samba..."
-sudo groupadd smbusers
-sudo usermod -aG smbusers $USER
-sudo smbpasswd -a $USER
-
-# Configure light
-echo "Configuring light..."
-sudo usermod -aG video $USER
-
-# Configure docker
-echo "Configuring docker..."
-sudo usermod -aG docker $USER
-
-# Install global dotnet tools
-echo "Installing global dotnet tools..."
-dotnet tool install --global dotnet-ef
-dotnet tool install --global csharprepl
-
-# Install dotnet templates
-echo "Installing dotnet templates..."
-dotnet new install Avalonia.Templates
-
-# Configure git: prompt for user.name
-echo "Please enter your Git user name:"
-read git_username
-git config --global user.name "$git_username"
-
-# Configure git: prompt for user.email
-echo "Please enter your Git email address:"
-read git_email
-git config --global user.email "$git_email"
-
-# Configure git: prompt for core.editor
-echo "Please enter your preferred text editor for Git (e.g., vim, nano, code):"
-read git_editor
-git config --global core.editor "$git_editor"
-
-# Configure git: confirmation
-echo "Git configuration updated:"
-echo "user.name: $(git config --global user.name)"
-echo "user.email: $(git config --global user.email)"
-echo "core.editor: $(git config --global core.editor)"
-
-# Clean cache
-echo "Cleaning cache..."
-sudo rm -rf ~/.cache/*
-
-# Perform clean-desktop
-echo "Cleaning desktop..."
-.bin/clean-desktop
-
-# Clean up AUR packages and cache
-echo "Cleaning up AUR packages and cache..."
-yay -Yc --noconfirm
-yay -Sc --noconfirm
-
+clean_up
 echo "Installation complete!"
